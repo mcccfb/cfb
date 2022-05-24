3fe8eb211f93ac05c88e2816a358b9eeaa8394e1
MCC1 <mcc@cabstand.com>
2022-05-24 10:26:44 -0700

    switch to fixed dates for most tests
    We want deterministic outputs for the tiebreak scenario unit
    tests so use a fixed date.

--- vconf/schedule_maker.py
+++ vconf/schedule_maker.py
@@ -10,6 +10,8 @@ from datetime import timedelta
 import constants
 import cfbd
 
+TEST_GAME_TIMESTAMP = 1653368232
+
 # fake a date in the CFBD format.
 # negative past means date is in the future.
 #
@@ -17,6 +19,15 @@ def create_date(days_in_past):
     today = date.today()
     delta = timedelta(days=days_in_past)
     gameday = today - delta
+    return gameday.strftime(constants.CFBD_DATE_FMT)
+
+# deterministic version that creates dates based on a defined constant
+# useful for deterministic testing output
+#
+def create_fixed_date(days_in_past):
+    baseline = date.fromtimestamp(TEST_GAME_TIMESTAMP)
+    delta = timedelta(days = days_in_past)
+    gameday = baseline - delta
     return gameday.strftime(constants.CFBD_DATE_FMT)
 
 def create_game(game_id, home_id, home_team, away_id, away_team, home_points, away_points, start_date):
@@ -31,6 +42,9 @@ def create_game(game_id, home_id, home_team, away_id, away_team, home_points, aw
     game.start_date = start_date
     return game
 
+# two teams, one teams wins everything.
+# team 1 should be the winner
+#
 def two_team_schedule():
     games = {}
     team1 = [ 24, 'Stanford']
@@ -38,7 +52,7 @@ def two_team_schedule():
     for game_id in range(1, 14):
         cur_game = cfbd.Game()
         cur_game.id = game_id
-        cur_game.start_date = create_date(100 - game_id)
+        cur_game.start_date = create_fixed_date(100 - game_id)
         cur_game.home_id = team1[0]
         cur_game.home_team = team1[1]
         cur_game.home_points = 24
@@ -69,7 +83,8 @@ def two_team_schedule_half_done():
     return games
 
 # torture test where three teams play each other and split
-# everything
+# everything. no winner possible
+#
 def three_team_tie():
     games = {}
     team1 = [ 24, 'Stanford']
@@ -77,20 +92,21 @@ def three_team_tie():
     team3 = [ 21, 'San Diego State' ]
 
     # team 1 beats the other two
-    games[1] = create_game(1, team1[0], team1[1], team2[0], team2[1], 30, 20, create_date(15))
-    games[2] = create_game(2, team1[0], team1[1], team3[0], team3[1], 30, 20, create_date(14))
+    games[1] = create_game(1, team1[0], team1[1], team2[0], team2[1], 30, 20, create_fixed_date(15))
+    games[2] = create_game(2, team1[0], team1[1], team3[0], team3[1], 30, 20, create_fixed_date(14))
 
     # team 2 gets two wins
-    games[3] = create_game(3, team2[0], team2[1], team1[0], team1[1], 30, 20, create_date(13))
-    games[4] = create_game(4, team2[0], team2[1], team3[0], team3[1], 30, 20, create_date(12))
+    games[3] = create_game(3, team2[0], team2[1], team1[0], team1[1], 30, 20, create_fixed_date(13))
+    games[4] = create_game(4, team2[0], team2[1], team3[0], team3[1], 30, 20, create_fixed_date(12))
 
     # team 3 gets two wins
-    games[5] = create_game(5, team3[0], team3[1], team1[0], team1[1], 30, 20, create_date(11))
-    games[6] = create_game(6, team3[0], team3[1], team2[0], team2[1], 30, 20, create_date(10))
+    games[5] = create_game(5, team3[0], team3[1], team1[0], team1[1], 30, 20, create_fixed_date(11))
+    games[6] = create_game(6, team3[0], team3[1], team2[0], team2[1], 30, 20, create_fixed_date(10))
 
     return games
 
 # two good teams one bad
+# should result in unresolveable two team tie
 #
 def two_team_tie_one_doormat():
     games = {}
@@ -99,16 +115,16 @@ def two_team_tie_one_doormat():
     team3 = [ 21, 'San Diego State' ]
 
     # team 1 beats the other two
-    games[1] = create_game(1, team1[0], team1[1], team2[0], team2[1], 30, 20, create_date(15))
-    games[2] = create_game(2, team1[0], team1[1], team3[0], team3[1], 30, 20, create_date(14))
+    games[1] = create_game(1, team1[0], team1[1], team2[0], team2[1], 30, 20, create_fixed_date(15))
+    games[2] = create_game(2, team1[0], team1[1], team3[0], team3[1], 30, 20, create_fixed_date(14))
 
     # team 2 gets two wins
-    games[3] = create_game(3, team2[0], team2[1], team1[0], team1[1], 30, 20, create_date(13))
-    games[4] = create_game(4, team2[0], team2[1], team3[0], team3[1], 30, 20, create_date(12))
+    games[3] = create_game(3, team2[0], team2[1], team1[0], team1[1], 30, 20, create_fixed_date(13))
+    games[4] = create_game(4, team2[0], team2[1], team3[0], team3[1], 30, 20, create_fixed_date(12))
 
     # team 3 loses to everyone
-    games[5] = create_game(5, team3[0], team3[1], team1[0], team1[1], 20, 30, create_date(11))
-    games[6] = create_game(6, team3[0], team3[1], team2[0], team2[1], 20, 30, create_date(10))
+    games[5] = create_game(5, team3[0], team3[1], team1[0], team1[1], 20, 30, create_fixed_date(11))
+    games[6] = create_game(6, team3[0], team3[1], team2[0], team2[1], 20, 30, create_fixed_date(10))
 
     return games
 
@@ -135,6 +151,9 @@ def real_life_future_schedule():
     
     return games
 
+# 4 teams end up with identical record.
+# should short circuit as a 4 team tie is not handled.
+#
 def four_team_tie():
     stanford = [ 24, 'Stanford']
     cal = [ 25, 'California' ]
@@ -145,18 +164,21 @@ def four_team_tie():
     fresno = [ 278, 'Fresno State' ]
 
     games = {}
-    games[1] = create_game(1, ucla[0], ucla[1], stanford[0], stanford[1], 30, 14, create_date(100))
-    games[2] = create_game(2, ucla[0], ucla[1], usc[0], usc[1], 31, 28, create_date(101))
-    games[3] = create_game(3, cal[0], cal[1], ucla[0], ucla[1], 24, 15, create_date(102))
-    games[4] = create_game(4, usc[0], usc[1], cal[0], cal[1], 8, 33, create_date(103))
-    games[5] = create_game(5, cal[0], cal[1], stanford[0], stanford[1], 12, 24, create_date(105))
-    games[6] = create_game(6, usc[0], usc[1], stanford[0], stanford[1], 20, 30, create_date(106))
-    games[7] = create_game(7, fresno[0], fresno[1], sjst[0], sjst[1], 40, 2, create_date(107))
-    games[8] = create_game(8, usc[0], usc[1], fresno[0], fresno[1], 8, 33, create_date(108))
-    games[9] = create_game(9, fresno[0], fresno[1], sdst[0], sdst[1], 18, 21, create_date(109))
-    games[10] = create_game(10, sdst[0], sdst[1], sjst[0], sjst[1], 20, 34, create_date(110))
+    games[1] = create_game(1, ucla[0], ucla[1], stanford[0], stanford[1], 30, 14, create_fixed_date(100))
+    games[2] = create_game(2, ucla[0], ucla[1], usc[0], usc[1], 31, 28, create_fixed_date(101))
+    games[3] = create_game(3, cal[0], cal[1], ucla[0], ucla[1], 24, 15, create_fixed_date(102))
+    games[4] = create_game(4, usc[0], usc[1], cal[0], cal[1], 8, 33, create_fixed_date(103))
+    games[5] = create_game(5, cal[0], cal[1], stanford[0], stanford[1], 12, 24, create_fixed_date(105))
+    games[6] = create_game(6, usc[0], usc[1], stanford[0], stanford[1], 20, 30, create_fixed_date(106))
+    games[7] = create_game(7, fresno[0], fresno[1], sjst[0], sjst[1], 40, 2, create_fixed_date(107))
+    games[8] = create_game(8, usc[0], usc[1], fresno[0], fresno[1], 8, 33, create_fixed_date(108))
+    games[9] = create_game(9, fresno[0], fresno[1], sdst[0], sdst[1], 18, 21, create_fixed_date(109))
+    games[10] = create_game(10, sdst[0], sdst[1], sjst[0], sjst[1], 20, 34, create_fixed_date(110))
     return games
 
+# 3 teams each lose one of the others in circular fashion
+# right now this produces a winner but should it?
+#
 def three_team_tie_circular_losses():
     stanford = [ 24, 'Stanford']
     cal = [ 25, 'California' ]
@@ -167,14 +189,14 @@ def three_team_tie_circular_losses():
     fresno = [ 278, 'Fresno State' ]
 
     games = {}
-    games[1] = create_game(1, ucla[0], ucla[1], stanford[0], stanford[1], 30, 14, create_date(100))
-    games[2] = create_game(2, ucla[0], ucla[1], usc[0], usc[1], 31, 28, create_date(101))
-    games[3] = create_game(3, cal[0], cal[1], ucla[0], ucla[1], 24, 15, create_date(102))
-    games[4] = create_game(4, usc[0], usc[1], cal[0], cal[1], 8, 33, create_date(103))
-    games[5] = create_game(5, cal[0], cal[1], stanford[0], stanford[1], 12, 24, create_date(105))
-    games[6] = create_game(6, usc[0], usc[1], stanford[0], stanford[1], 20, 30, create_date(106))
-    games[7] = create_game(7, fresno[0], fresno[1], sjst[0], sjst[1], 40, 2, create_date(107))
-    games[8] = create_game(8, usc[0], usc[1], fresno[0], fresno[1], 33, 23, create_date(108))
-    games[9] = create_game(9, fresno[0], fresno[1], sdst[0], sdst[1], 18, 21, create_date(109))
-    games[10] = create_game(10, sdst[0], sdst[1], sjst[0], sjst[1], 20, 34, create_date(110))
+    games[1] = create_game(1, ucla[0], ucla[1], stanford[0], stanford[1], 30, 14, create_fixed_date(100))
+    games[2] = create_game(2, ucla[0], ucla[1], usc[0], usc[1], 31, 28, create_fixed_date(101))
+    games[3] = create_game(3, cal[0], cal[1], ucla[0], ucla[1], 24, 15, create_fixed_date(102))
+    games[4] = create_game(4, usc[0], usc[1], cal[0], cal[1], 8, 33, create_fixed_date(103))
+    games[5] = create_game(5, cal[0], cal[1], stanford[0], stanford[1], 12, 24, create_fixed_date(105))
+    games[6] = create_game(6, usc[0], usc[1], stanford[0], stanford[1], 20, 30, create_fixed_date(106))
+    games[7] = create_game(7, fresno[0], fresno[1], sjst[0], sjst[1], 40, 2, create_fixed_date(107))
+    games[8] = create_game(8, usc[0], usc[1], fresno[0], fresno[1], 33, 23, create_fixed_date(108))
+    games[9] = create_game(9, fresno[0], fresno[1], sdst[0], sdst[1], 18, 21, create_fixed_date(109))
+    games[10] = create_game(10, sdst[0], sdst[1], sjst[0], sjst[1], 20, 34, create_fixed_date(110))
     return games
